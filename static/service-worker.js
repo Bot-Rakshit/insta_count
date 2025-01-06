4cf1a93468be2599c2a6fde1d579709c7a40f8ed
Bot-Rakshit <singhrakshit003@gmail.com>
2025-01-06 23:00:59 +0530

    foruth card

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                '/amishi',
+                '/amishi/',
                 '/static/manifest.json',
                 '/static/icon-192x192.png',
                 '/static/icon-512x512.png',
@@ -13,6 +13,11 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+    if (event.request.url.endsWith('/amishi')) {
+        event.respondWith(Response.redirect('/amishi/'));
+        return;
+    }
+
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
